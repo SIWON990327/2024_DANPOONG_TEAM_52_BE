2ba309656e3968032c20c329a9c44469b1ea592e
koosco <rnxogud123@naver.com>
2024-11-19 01:59:34 +0900

    refactor: 도메인 의존성 분리
    Job 도메인에서 Member에 대한 의존성 분리

--- src/main/java/com/groom/orbit/job/app/JobCommandService.java
+++ src/main/java/com/groom/orbit/job/app/JobCommandService.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Service;
 import com.groom.orbit.common.dto.CommonSuccessDto;
 import com.groom.orbit.job.app.dto.InterestJobRequestDto;
 import com.groom.orbit.job.dao.jpa.entity.Job;
-import com.groom.orbit.member.app.MemberQueryService;
-import com.groom.orbit.member.dao.jpa.entity.Member;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,19 +14,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JobCommandService {
 
-  private final MemberQueryService memberQueryService;
   private final JobQueryService jobQueryService;
   private final InterestJobService interestJobService;
 
   public CommonSuccessDto saveInterestJob(Long memberId, InterestJobRequestDto dto) {
-    Member member = getMember(memberId);
     List<Job> jobs = jobQueryService.findJobsByIds(dto.ids());
-    interestJobService.saveInterestJob(member, jobs);
+    interestJobService.saveInterestJob(memberId, jobs);
 
     return CommonSuccessDto.fromEntity(true);
-  }
-
-  private Member getMember(Long memberId) {
-    return memberQueryService.findMember(memberId);
   }
 }
