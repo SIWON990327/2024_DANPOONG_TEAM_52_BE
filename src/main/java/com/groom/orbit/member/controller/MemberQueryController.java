fa9e498fc36589f9095969c28b5a746a28aa9d0e
SIWON990327 <pos06058@naver.com>
2024-11-20 22:40:36 +0900

    feat : 사용자 이미지, 닉네임 조회

---
+++ src/main/java/com/groom/orbit/member/controller/MemberQueryController.java
@@ -0,0 +1,25 @@
+package com.groom.orbit.member.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.groom.orbit.common.annotation.AuthMember;
+import com.groom.orbit.common.dto.ResponseDto;
+import com.groom.orbit.member.app.MemberQueryService;
+import com.groom.orbit.member.app.dto.response.GetMemberProfileResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberQueryController {
+
+  private final MemberQueryService memberQueryService;
+
+  @GetMapping
+  public ResponseDto<GetMemberProfileResponseDto> getMemberProfile(@AuthMember Long memberId) {
+    return ResponseDto.ok(memberQueryService.getMemberProfile(memberId));
+  }
+}
