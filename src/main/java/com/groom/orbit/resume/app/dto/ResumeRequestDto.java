daf55dbc3f311a80273c9923b8888595fd061cce
koosco <rnxogud123@naver.com>
2024-11-23 17:42:37 +0900

    fix: LocalDateType으로 타입 수정

--- src/main/java/com/groom/orbit/resume/app/dto/ResumeRequestDto.java
+++ src/main/java/com/groom/orbit/resume/app/dto/ResumeRequestDto.java
@@ -1,13 +1,17 @@
 package com.groom.orbit.resume.app.dto;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import com.groom.orbit.member.dao.jpa.entity.Member;
 import com.groom.orbit.resume.dao.entity.Resume;
 import com.groom.orbit.resume.dao.entity.ResumeCategory;
 
 public record ResumeRequestDto(
-    ResumeCategory resumeCategory, String title, String content, Date startDate, Date endDate) {
+    ResumeCategory resumeCategory,
+    String title,
+    String content,
+    LocalDate startDate,
+    LocalDate endDate) {
 
   public Resume toResume(Member member) {
     return Resume.builder()
--- src/main/java/com/groom/orbit/resume/dao/entity/Resume.java
+++ src/main/java/com/groom/orbit/resume/dao/entity/Resume.java
@@ -1,8 +1,18 @@
 package com.groom.orbit.resume.dao.entity;
 
-import java.util.Date;
+import java.time.LocalDate;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import com.groom.orbit.member.dao.jpa.entity.Member;
 import com.groom.orbit.resume.app.dto.ResumeRequestDto;
@@ -25,21 +35,21 @@ public class Resume {
   @Column(name = "resume_id")
   private Long resumeId;
 
-  @Column(name = "category")
+  @Column(name = "category", length = 10)
   @Enumerated(EnumType.STRING)
   private ResumeCategory resumeCategory;
 
-  @Column(name = "title")
+  @Column(name = "title", length = 50)
   private String title;
 
-  @Column(name = "content")
+  @Column(name = "content", length = 50)
   private String content;
 
   @Column(name = "start_date")
-  private Date startDate;
+  private LocalDate startDate;
 
   @Column(name = "end_date")
-  private Date endDate;
+  private LocalDate endDate;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "member_id")
