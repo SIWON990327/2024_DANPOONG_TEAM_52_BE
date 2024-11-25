b919bcfb3a4bd2109334d38381d2a5ff0473aa2b
koosco <rnxogud123@naver.com>
2024-11-25 21:43:24 +0900

    fix: ddl문 수정

--- sql/init.sql
+++ sql/init.sql
@@ -14,14 +14,14 @@ DROP TABLE IF EXISTS member;
 -- member 테이블을 먼저 생성
 CREATE TABLE member
 (
-    member_id       BIGINT        NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nickname        VARCHAR(100)  NOT NULL,
-    kakao_nickname        VARCHAR(100)  NOT NULL,
-    image_url       VARCHAR(500)  NOT NULL,
-    known_prompt    VARCHAR(1000) NOT NULL DEFAULT '',
-    help_prompt     VARCHAR(1000) NOT NULL DEFAULT '',
-    is_notification BOOLEAN       NOT NULL DEFAULT FALSE,
-    is_profile      BOOLEAN       NOT NULL DEFAULT FALSE,
+    member_id       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nickname        VARCHAR(100) NOT NULL,
+    kakao_nickname  VARCHAR(100) NOT NULL,
+    image_url       VARCHAR(500) NOT NULL,
+    known_prompt    TEXT         NOT NULL DEFAULT '',
+    help_prompt     TEXT         NOT NULL DEFAULT '',
+    is_notification BOOLEAN      NOT NULL DEFAULT FALSE,
+    is_profile      BOOLEAN      NOT NULL DEFAULT FALSE,
     ai_feedback     TEXT
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
@@ -47,13 +47,13 @@ CREATE TABLE interest_job
 
 CREATE TABLE resume
 (
-    resume_id  BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title      VARCHAR(50) NOT NULL,
-    category   VARCHAR(50) NOT NULL,
-    content    VARCHAR(50) NOT NULL,
+    resume_id  BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title      VARCHAR(100) NOT NULL,
+    category   VARCHAR(50)  NOT NULL,
+    content    VARCHAR(100),
     start_date DATE,
     end_date   DATE,
-    member_id  BIGINT      NOT NULL,
+    member_id  BIGINT       NOT NULL,
     CONSTRAINT fk_resume_member FOREIGN KEY (member_id) REFERENCES member (member_id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
@@ -69,11 +69,11 @@ CREATE TABLE goal
 
 CREATE TABLE member_goal
 (
-    member_goal_id BIGINT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id      BIGINT  NOT NULL,
-    created_at  DATETIME    NOT NULL,
-    goal_id        BIGINT  NOT NULL,
-    is_complete    BOOLEAN NOT NULL DEFAULT FALSE,
+    member_goal_id BIGINT   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    created_at     DATETIME NOT NULL,
+    member_id      BIGINT   NOT NULL,
+    goal_id        BIGINT   NOT NULL,
+    is_complete    BOOLEAN  NOT NULL DEFAULT FALSE,
     CONSTRAINT member_goal_member FOREIGN KEY (member_id) REFERENCES member (member_id),
     CONSTRAINT member_goal_goal FOREIGN KEY (goal_id) REFERENCES goal (goal_id)
 ) ENGINE = InnoDB
@@ -81,13 +81,13 @@ CREATE TABLE member_goal
 
 CREATE TABLE quest
 (
-    quest_id    BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title       VARCHAR(50) NOT NULL,
-    is_complete BOOLEAN     NOT NULL DEFAULT FALSE,
-    created_at  DATETIME    NOT NULL,
-    deadline    DATE        NOT NULL,
-    sequence    INT         NOT NULL,
-    member_goal_id   BIGINT      NOT NULL,
+    quest_id       BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title          VARCHAR(50) NOT NULL,
+    is_complete    BOOLEAN     NOT NULL DEFAULT FALSE,
+    created_at     DATE        NOT NULL,
+    deadline       DATE        NOT NULL,
+    sequence       INT         NOT NULL,
+    member_goal_id BIGINT      NOT NULL,
     CONSTRAINT fk_quest_member_goal FOREIGN KEY (member_goal_id) REFERENCES member_goal (member_goal_id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
