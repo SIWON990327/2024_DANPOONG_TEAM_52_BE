45b959c2a309f23ca950d10a85d35c58bc54c8ff
김시원 / Kim Siwon <114036799+SIWON990327@users.noreply.github.com>
2024-11-29 22:58:06 +0900

    refactor : 관심 직업 변경 (#203)

--- src/main/java/com/groom/orbit/job/app/JobCommandService.java
+++ src/main/java/com/groom/orbit/job/app/JobCommandService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import com.groom.orbit.common.dto.CommonSuccessDto;
 import com.groom.orbit.job.app.dto.InterestJobRequestDto;
+import com.groom.orbit.job.dao.jpa.InterestJobRepository;
+import com.groom.orbit.job.dao.jpa.entity.InterestJob;
 import com.groom.orbit.job.dao.jpa.entity.Job;
 
 import lombok.RequiredArgsConstructor;
@@ -16,8 +18,14 @@ public class JobCommandService {
 
   private final JobQueryService jobQueryService;
   private final InterestJobService interestJobService;
+  private final InterestJobRepository interestJobRepository;
 
   public CommonSuccessDto saveInterestJob(Long memberId, InterestJobRequestDto dto) {
+
+    List<InterestJob> interestJobList = interestJobRepository.findAllByMemberId(memberId);
+
+    interestJobRepository.deleteAll(interestJobList);
+
     List<Job> jobs = jobQueryService.findJobsByIds(dto.ids());
     interestJobService.saveInterestJob(memberId, jobs);
 
