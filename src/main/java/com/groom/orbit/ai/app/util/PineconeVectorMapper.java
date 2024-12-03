4cf90a172335808edb0e19ee0e134af6a5653052
koosco <rnxogud123@naver.com>
2024-12-04 07:48:55 +0900

    refactor: vector db metadata format 변경
    json 형태로 일관성있게 metadata를 저장할 수 있도록 변경했습니다.

--- src/main/java/com/groom/orbit/ai/app/util/PineconeVectorMapper.java
+++ src/main/java/com/groom/orbit/ai/app/util/PineconeVectorMapper.java
@@ -1,15 +1,10 @@
 package com.groom.orbit.ai.app.util;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.protobuf.Struct;
-import com.google.protobuf.Value;
 import com.groom.orbit.ai.dao.vector.Vector;
 import com.groom.orbit.common.exception.CommonException;
 import com.groom.orbit.common.exception.ErrorCode;
@@ -23,32 +18,20 @@ public class PineconeVectorMapper {
     this.mapper = new ObjectMapper();
   }
 
-  public String mapToString(Vector object) {
+  public String mapToString(Vector vector) {
     try {
-      return mapper.writeValueAsString(object);
+      return mapper.writeValueAsString(vector);
     } catch (JsonProcessingException e) {
       throw new CommonException(ErrorCode.INVALID_STATE);
     }
   }
 
   public Vector fromStruct(Struct struct) {
-    Map<String, Value> fields = struct.getFieldsMap();
-
-    Long memberId = Long.parseLong(fields.get("memberId").getStringValue());
-    String memberName = fields.get("memberName").getStringValue();
-    List<String> interestJobs =
-        convertJsonToList(fields.get("interestJobs").getStringValue(), mapper);
-    List<String> goals = convertJsonToList(fields.get("goals").getStringValue(), mapper);
-    List<String> quests = convertJsonToList(fields.get("quests").getStringValue(), mapper);
-
-    return new Vector(memberId, memberName, interestJobs, goals, quests);
-  }
-
-  private List<String> convertJsonToList(String jsonString, ObjectMapper mapper) {
     try {
-      return mapper.readValue(jsonString, new TypeReference<List<String>>() {});
+      String jsonString = struct.getFieldsMap().get("metadata").getStringValue();
+      return new ObjectMapper().readValue(jsonString, Vector.class);
     } catch (JsonProcessingException e) {
-      throw new CommonException(ErrorCode.INVALID_STATE);
+      throw new RuntimeException(e);
     }
   }
 }
