863b4ca0c3df8433278a3c4d8115285a3c787734
koosco <rnxogud123@naver.com>
2024-12-06 04:54:20 +0900

    fix: 탐색 pageable default size 10으로 설정

--- src/main/java/com/groom/orbit/goal/controller/query/GoalSearchController.java
+++ src/main/java/com/groom/orbit/goal/controller/query/GoalSearchController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,7 +37,7 @@ public class GoalSearchController {
       @AuthMember Long memberId,
       @RequestParam(value = "category", required = false) String category,
       @RequestParam(value = "jobIds", required = false) List<Long> jobIds,
-      Pageable pageable) {
+      @PageableDefault(size = 10) Pageable pageable) {
     return ResponseDto.ok(goalSearchService.searchGoals(memberId, category, jobIds, pageable));
   }
 }
